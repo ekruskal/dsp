4267a9eac367d522a2d7379eb65fd087d9154947
ekruskal <Elliot@kruskal.net>
2016-03-29 13:39:10 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+def write_data(data):
+    with open('emails.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for i in data:
+            writer.writerow([i])
+    return 'emails.csv'
+write_data(email_addresses)
